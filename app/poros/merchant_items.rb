2fadfa6dae147f32d5ac14689774aca87b4087f6
Austin Woods <austinwoods99@gmail.com>
2023-07-20 13:55:00 -0400

    finsih

---
+++ app/poros/merchant_items.rb
@@ -0,0 +1,6 @@
+class MerchantItems
+  attr_reader :data
+  def initialize(data)
+    @data = data[:data]
+  end
+end
---
+++ app/poros/merchants.rb
@@ -0,0 +1,6 @@
+class Merchants
+  attr_reader :data
+  def initialize(data)
+    @data = data[:data]
+  end
+end
